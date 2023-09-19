68a5dede9f3d81db0e154e80c1b171fc3a7624c1
Jorge Sandoval <jrss317031@gmail.com>
2023-09-19 16:57:39 -0500

    src/app/app.component.html src/app/app.component.ts src/app/empleado/empleado.component.html src/app/empleado/empleado.component.ts src/app/empleados/empleados.component.html src/app/empleados/empleados.component.ts src/index.html

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-empleado',
+selector: 'app-empleado',
 templateUrl: './empleado.component.html',
  // template:"<p>Aquí iría un empleado👌👌👌</p>",
 styleUrls: ['./empleado.component.css']
  // styles:["p{ background-color: red;}"]
 
 })
-export class EmpleadoComponent {
+export class EmpleadoComponent implements OnInit {
+
+  nombre="Juan";
+
+  apellido="Diaz";
+
+  edad=18;
+
+  // empresa="Píldoras Informáticas";
+
+  /* getEdad(){
+
+    return this.edad;
+  } */
+
+  habilitacionCuadro=true; 
+
+  usuRegistrado=false; 
+
+  textoDeRegistro="No hay nadie registrado";
+
+
+  getRegistroUsuario(){
+
+    this.usuRegistrado=false;
+  }
+
+  setusuarioRegistrado(event:Event){
+
+    // alert ("El usuario se acaba de registrar");
+
+    // this.textoDeRegistro="El usuario se acaba de registrar"; 
+
+    if((<HTMLInputElement>event.target).value=="Sí"){
+      this.textoDeRegistro="El usuario se acaba de registrar"; 
+    }else{
+      this. textoDeRegistro="No hay nadie registrado";
+    }
+
+  }
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
